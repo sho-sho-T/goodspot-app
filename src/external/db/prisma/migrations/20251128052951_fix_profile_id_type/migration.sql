093242308bd4e1ad195e600a8584f4cc6b426d7e
ShotoMatsubara <shoto0628matsubara@gmail.com>
2025-11-28 15:37:09 +0900

    profilesテーブルを追加

---
+++ src/external/db/prisma/migrations/20251128052951_fix_profile_id_type/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" UUID NOT NULL,
+    "displayName" TEXT,
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
