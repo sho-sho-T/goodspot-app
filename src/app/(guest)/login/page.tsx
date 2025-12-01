d4669dd5d6447f9588c5a531d7f5104fe6f935f1
ShotoMatsubara <shoto0628matsubara@gmail.com>
2025-12-01 22:24:15 +0900

    ログインページコンポーネントを追加

---
+++ src/app/(guest)/login/page.tsx
@@ -0,0 +1,7 @@
+import { LoginPageTemplate } from '@/features/auth/components/server/LoginPageTemplate'
+
+const LoginPage = (_props: PageProps<'/login'>) => {
+  return <LoginPageTemplate />
+}
+
+export default LoginPage
